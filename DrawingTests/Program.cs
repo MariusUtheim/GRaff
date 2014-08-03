89e103826558361e1118936b529164e405ddce29
Marius Utheim <marius_utheim@hotmail.com>
2014-08-03 17:08:11 +0200

    Committed a lot of stuff

---
+++ DrawingTests/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GameMaker;
+
+namespace DrawingTests
+{
+	static class Program
+	{
+		static void Main()
+		{
+			Game.Run<GameMaker.Forms.FormsGraphicsEngine>(gameStart);
+		}
+
+		static void gameStart()
+		{
+			GlobalEvent.ExitOnEscape();
+			Window.IsBorderVisible = false;
+
+			Instance<Drawer>.Create();
+			Instance<Block>.Create(400, 400);
+		}
+	}
+}
