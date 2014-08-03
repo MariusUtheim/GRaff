c432a6c26e912555d87c42377268d0135c5919d6
Marius Utheim <marius_utheim@hotmail.com>
2014-08-03 16:57:25 +0200

    Added Complex and Window classes

---
+++ GameMaker/Complex.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameMaker
+{
+	public struct Complex
+	{
+		private double _r, _i;
+
+		public Complex(double r, double i)
+		{
+			this._r = r;
+			this._i = i;
+		}
+
+		public double Real
+		{
+			get { return _r; }
+			set { _r = value; }
+		}
+
+		public double Imaginary
+		{
+			get { return _i; }
+			set { _i = value; }
+		}
+
+		public static Complex Polar(double magnitude, double argument)
+		{
+			return new Complex(magnitude * GMath.Cos(argument), magnitude * GMath.Sin(argument));
+		}
+
+
+		public static Complex operator +(Complex z, Complex w)
+		{
+			return new Complex(z._r + w._r, z._i + w._i);
+		}
+
+		public static Complex operator -(Complex z, Complex w)
+		{
+			return new Complex(z._r - w._r, z._i - w._i);
+		}
+
+		public static Complex operator *(Complex z, Complex w)
+		{
+			return new Complex(z._r * w._r - z._i * w._i, z._r * w._i + z._i * w._r);
+		}
+
+		public static Complex operator /(Complex z, Complex w)
+		{
+			double m = 1 / (w._r * w._r + w._i * w._i);
+			return new Complex(m * (z._r * w._r + z._i * w._i), m * (z._i * w._r - z._r * w._i));
+		}
+
+		public static implicit operator Complex(double d)
+		{
+			return new Complex(d, 0);
+		}
+
+		public static explicit operator Complex(Point p)
+		{
+			return new Complex(p.X, p.Y);
+		}
+	}
+}
---
+++ GameMaker/Window.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameMaker
+{
+	public static class Window
+	{
+		public static bool IsBorderVisible
+		{
+			get { return GraphicsEngine.Current.IsBorderVisible; }
+			set { GraphicsEngine.Current.IsBorderVisible = value; }
+		}
+	}
+}
