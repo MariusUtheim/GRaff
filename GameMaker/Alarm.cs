06afb5caa49aee718eb824f0304db817584ca709
Marius Utheim <marius_utheim@hotmail.com>
2014-09-28 23:05:44 +0200

    Minor changes to Alarm

--- GameMaker/Alarm.cs
+++ GameMaker/Alarm.cs
@@ -15,7 +15,7 @@ namespace GameMaker
 	}
 
 	/// <summary>
-	/// Ticks down every step, and fires an event when the 
+	/// Ticks down every step, and fires an event when the tick reaches zero.
 	/// </summary>
 	public sealed class Alarm
 	{
@@ -25,6 +25,12 @@ namespace GameMaker
 
 		public Alarm()
 		{
+			_alarms.Add(this);
+		}
+
+		~Alarm()
+		{
+			_alarms.Remove(this);
 		}
 
 		public static Alarm Start(int count, EventHandler<AlarmEventArgs> action)
@@ -43,26 +49,48 @@ namespace GameMaker
 				a.Tick();
 		}
 
+		/// <summary>
+		/// Gets or sets the initial count of this GameMaker.Alarm. This will contain the value passed to GameMaker.Alarm.Restart(int)
+		/// </summary>
 		public int InitialCount { get; set; }
 
+		/// <summary>
+		/// Gets or sets the number of ticks remaining until this GameMaker.Alarm fires the alarm event.
+		/// </summary>
 		public int Count { get; set; }
 
-
+		/// <summary>
+		/// Gets the state of this GameMaker.Alarm.
+		/// </summary>
 		public AlarmState State { get; private set; }
 
+		/// <summary>
+		/// Gets or sets whether this GameMaker.Alarm should be automatically loop.
+		/// Whenever the countdown for this GameMaker.Alarm reaches zero, if IsLooping is set to true, the countdown is reset to the value of InitialCount.
+		/// </summary>
 		public bool IsLooping { get; set; }
 
+		/// <summary>
+		/// Resets the countdown of this GameMaker.Alarm to the value of InitialCount.
+		/// If InitialCount is less than or equal to zero, this GameMaker.Alarm is stopped instead.
+		/// </summary>
 		public void Restart()
 		{
 			Count = InitialCount;
 			Start();	
 		}
 
+		/// <summary>
+		/// Resets the countdown of this GameMaker.Alarm to the specified initial count.
+		/// If count is less than or equal to zero, this GameMaker.Alarm is stopped instead.
+		/// </summary>
+		/// <param name="count">The initial countdown.</param>
 		public void Restart(int count)
 		{
 			Count = InitialCount = count;
 			Start();
 		}
+
 
 		public void Start()
 		{
@@ -92,7 +120,7 @@ namespace GameMaker
 		}
 
 		/// <summary>
-		/// Reduces the alarm by 1 if it is running.
+		/// Reduces the countdown of this GameMaker.Alarm if it is running, and fires the alarm event if countdown reaches zero.
 		/// </summary>
 		public void Tick()
 		{
@@ -105,7 +133,7 @@ namespace GameMaker
 		}
 
 		/// <summary>
-		/// Triggers this alarm once, without changing the countdown.
+		/// Fires the alarm event of this GameMaker.Alarm without changing the countdown.
 		/// </summary>
 		public void Trigger()
 		{
@@ -113,7 +141,7 @@ namespace GameMaker
 		}
 
 		/// <summary>
-		/// Triggers this alarm. If IsLooping is set to true it will loop; otherwise the countdown will be set to zero and it is stopped.
+		/// Fires the alarm event of this GameMaker.Alarm. If IsLooping is set to true, the countdown is reset to InitialValue; otherwise the countdown will be set to zero and it is stopped.
 		/// </summary>
 		public void Complete()
 		{
--- GameMaker/AlarmEventArgs.cs
+++ GameMaker/AlarmEventArgs.cs
@@ -2,14 +2,23 @@
 
 namespace GameMaker
 {
+	/// <summary>
+	/// Defines event data for an alarm event.
+	/// </summary>
 	public class AlarmEventArgs : EventArgs
 	{
-
+		/// <summary>
+		/// Initializes a new instance of the GameMaker.AlarmEventArgs class using the specified GameMaker.Alarm.
+		/// </summary>
+		/// <param name="alarm">The alarm associated with the event.</param>
 		public AlarmEventArgs(Alarm alarm)
 		{
 			this.Alarm = alarm;
 		}
 
-		public Alarm Alarm { get; set; }
+		/// <summary>
+		/// Gets the GameMaker.Alarm associated with the event.
+		/// </summary>
+		public Alarm Alarm { get; private set; }
 	}
 }
