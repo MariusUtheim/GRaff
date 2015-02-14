e63d7e7e0426ad9c48b3d516a4e71693fe909745
Marius Utheim <marius_utheim@hotmail.com>
2015-02-14 17:42:45 +0100

    Added RotatingFeature

---
+++ GRaff/Particles/Behaviors/RotatingFeature.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GRaff.Randomness;
+
+namespace GRaff.Particles.Behaviors
+{
+	public class RotatingFeature : IParticleFeature
+	{
+		private IDistribution<Angle> _direction;
+
+		public RotatingFeature()
+			: this(new AngleDistribution())
+		{ }
+
+
+		public RotatingFeature(IDistribution<Angle> initialDirection)
+		{
+			if (initialDirection == null) throw new ArgumentNullException("initialDirection");	 /*C#6.0*/
+			_direction = initialDirection;
+		}
+
+		public void AttachTo(Particle particle)
+		{
+			particle.AttachBehavior(new RotatingBehavior(_direction.Generate()));
+		}
+	}
+}
