638fa2f698189e40153cc8fd33757e38957df439
Marius Utheim <marius_utheim@hotmail.com>
2015-02-25 01:43:00 +0100

    Added Synchronization.Coroutine

---
+++ TestGame/CoroutineTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GRaff;
+using GRaff.Synchronization;
+
+namespace TestGame
+{
+	class CoroutineTest : GameElement, IKeyPressListener, IKeyReleaseListener
+	{
+		private Coroutine _routine = null;
+
+		public CoroutineTest()
+		{
+		}
+
+		public void OnKeyRelease(Key key)
+		{
+			if (_routine != null)
+			{
+				_routine.Wait();
+				_routine = null;
+			}
+		}
+
+		public void OnKeyPress(Key key)
+		{
+			if (_routine == null)
+				_routine = Coroutine.Start(coroutine);
+		}
+
+		private IEnumerable<int> coroutine()
+		{
+			Window.Title = "1";
+			yield return 10;
+			Window.Title = "2";
+			yield return 60;
+			Window.Title = "3";
+			yield return 120;
+			Window.Title = "<< Done >>";
+		}
+	}
+}
