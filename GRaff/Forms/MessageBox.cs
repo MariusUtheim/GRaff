f49aa9b47349a96c82345619306dbeb41d20e7ec
Marius Utheim <marius_utheim@hotmail.com>
2015-04-15 20:37:13 +0200

    Added Forms.MessageBox and Forms.OpenFileDialog

---
+++ GRaff/Forms/MessageBox.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using WinMessageBox = System.Windows.Forms.MessageBox;
+
+namespace GRaff.Forms
+{
+	public class MessageBox
+	{
+		public static void Show(string text)
+		{
+			WinMessageBox.Show(text);
+		}
+	}
+}
