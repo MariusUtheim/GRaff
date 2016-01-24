061dd92ed6b4abe37270016744e9a207e88bdd74
Marius Utheim <marius_utheim@hotmail.com>
2016-01-24 02:30:28 +0100

    Project template initial commit

---
+++ GRaff.ProjectTemplate/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GRaff;
+
+namespace GRaff.ProjectTemplate
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		static void Main()
+		{
+			Giraffe.Run(1280, 768, gameStart);
+		}
+
+		/// <summary>
+		/// Initialize yoru game here.
+		/// </summary>
+		static void gameStart()
+		{
+
+		}
+	}
+}
