b5c945707679000bfa4e5c23216443533ec171ae
Marius Utheim <marius_utheim@hotmail.com>
2016-01-25 01:23:27 +0100

    .

--- GRaff.XmlAssets/Assets.cs
+++ GRaff.XmlAssets/Assets.cs
@@ -6,14 +6,10 @@ namespace TestGame.Assets
 	{
 		static Sprites()
 		{
-			Particle = new global::GRaff.Sprite(@"Star.png", 1, new global::GRaff.IntVector(12, 13), global::GRaff.MaskShape.Rectangle(50, 50, 400, 400), new global::GRaff.AnimationStrip(new global::System.Tuple<int, double>(0, 1), new global::System.Tuple<int, double>(4, 0.5)));
-			Xujia = new global::GRaff.Sprite(@"Xujia.jpg", 1, new global::GRaff.IntVector(0, 0), null, new global::GRaff.AnimationStrip(1));
 		}
 
 		public static void LoadAll()
 		{
-			global::GRaff.GRaffExtensions.Load(Particle);
-			global::GRaff.GRaffExtensions.Load(Xujia);
 		}
 
 		public static global::GRaff.Sprite Particle { get; private set; }
@@ -44,7 +40,6 @@ namespace TestGame.Assets
 		static Batches()
 		{
 			MainMenu = new global::GRaff.AssetBatch();
-			Everything = new global::GRaff.AssetBatch(global::TestGame.Assets.Sprites.Particle, global::TestGame.Assets.Sprites.Xujia, global::TestGame.Assets.Sounds.theSound, global::TestGame.Assets.Sounds.theOtherSound, global::TestGame.Assets.Batches.MainMenu);
 		}
 
 		public static void LoadAll()
