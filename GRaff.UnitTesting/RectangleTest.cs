f0747ee795c495a403f5fdf45d5ae128012b4a8c
Marius Utheim <marius_utheim@hotmail.com>
2018-09-11 22:01:38 +0200

    add more Rectangle tests

--- GRaff.UnitTesting/RectangleTest.cs
+++ GRaff.UnitTesting/RectangleTest.cs
@@ -7,6 +7,8 @@ namespace GRaff.UnitTesting
 	[TestClass]
 	public class RectangleTest
 	{
+        Random rand = new Random();
+
 		[TestMethod]
 		public void IntRectangleIntersection()
 		{
@@ -26,5 +28,42 @@ namespace GRaff.UnitTesting
 			rec2 = new IntRectangle(50, 50, 40, 40);
 			Assert.AreEqual(null, rec1.Intersection(rec2));
 		}
+
+        [TestMethod]
+        public void RectangleIntersections()
+        {
+            var unitRect = new Rectangle(0, 0, 1, 1);
+
+            var offsetRect = unitRect + new Vector(rand.Double(), rand.Double());
+            Assert.IsTrue(unitRect.Intersects(offsetRect));
+
+            var intersection = unitRect.Intersection(offsetRect).Value;
+            Assert.AreEqual(new Rectangle(offsetRect.TopLeft, new Vector(1 - offsetRect.Left, 1 - offsetRect.Top)), intersection);
+            
+            var nonIntersection = new Rectangle(1 + rand.Double(), 1 + rand.Double(), 1, 1);
+            Assert.IsFalse(unitRect.Intersects(nonIntersection));
+            Assert.IsNull(unitRect.Intersection(nonIntersection));
+        }
+
+        [TestMethod]
+        public void RectangleBoundaryIntersections()
+        {
+            var rect = new Rectangle(0, 0, rand.Double(), rand.Double());
+
+            Assert.IsFalse(rect.Intersects(new Rectangle(rect.Width, 0, 1, 1)));
+            Assert.IsFalse(rect.Intersects(new Rectangle(0, rect.Height, 1, 1)));
+
+            Assert.IsFalse(rect.Contains(new Point(rect.Width, rand.Double(rect.Height))));
+            Assert.IsFalse(rect.Contains(new Point(rand.Double(rect.Width), rect.Height)));
+            Assert.IsTrue(rect.Contains(new Point(0, rand.Double(rect.Height))));
+            Assert.IsTrue(rect.Contains(new Point(rand.Double(rect.Width), 0)));
+
+        }
+
+        [TestMethod]
+        public void RectangleNegativeIntersections()
+        {
+
+        }
 	}
 }
