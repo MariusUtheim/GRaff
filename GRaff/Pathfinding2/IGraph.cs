b24ced99ad8ff7262165c73d724eefa852850bcf
Marius Utheim <marius_utheim@hotmail.com>
2019-08-25 16:12:08 +0200

    Include new framework for pathfinding

---
+++ GRaff/Pathfinding2/IGraph.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GRaff.Pathfinding2
+{
+    interface IGraph
+    {
+        IEnumerable<IVertex> Vertices { get; }
+    }
+}
