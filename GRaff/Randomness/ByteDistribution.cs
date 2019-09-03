666f7a47e4f734007ccc1aec1ddaaefb28cefdef
marius <Marius_Utheim@hotmail.com>
2019-09-03 11:06:55 +0200

    Make RgbDistribution more flexible

---
+++ GRaff/Randomness/ByteDistribution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GRaff.Randomness
+{
+    /// <summary>
+    /// Generator for bytes according to a uniform distribution.
+    /// </summary>
+	public sealed class ByteDistribution : IDistribution<byte>
+    {
+        private readonly Random _rnd;
+
+        public ByteDistribution()
+            : this(GRandom.Source) { }
+
+        public ByteDistribution(Random rnd)
+        {
+            _rnd = rnd;
+        }
+
+        public byte Generate()
+        {
+            return _rnd.Byte();
+        }
+    }
+}
--- GRaff/Randomness/RgbDistribution.cs
+++ GRaff/Randomness/RgbDistribution.cs
@@ -6,23 +6,33 @@ namespace GRaff.Randomness
     /// <summary>
     /// Generator for a color distribution where the red, green and blue channels are independently distributed. 
     /// </summary>
-    #warning Add more constructors
     public sealed class RgbDistribution : IDistribution<Color>
 	{
-		private readonly Random _rnd;
-
 		public RgbDistribution()
-			: this(GRandom.Source) { }
+			: this(GRandom.Source)
+        { }
 
 		public RgbDistribution(Random rnd)
-		{
-			Contract.Requires<ArgumentNullException>(rnd != null);
-			_rnd = rnd;
-		}
+            : this(new ByteDistribution(rnd), new ByteDistribution(rnd), new ByteDistribution(rnd))
+		{ }
+
+        public RgbDistribution(IDistribution<byte> red, IDistribution<byte> green, IDistribution<byte> blue)
+        {
+            this.Red = red;
+            this.Green = green;
+            this.Blue = blue;
+        }
+
+        public IDistribution<byte> Red { get; set; }
+
+        public IDistribution<byte> Green { get; set; }
+
+        public IDistribution<byte> Blue { get; set; }
+
 
 		public Color Generate()
 		{
-			return _rnd.Color();
+            return Color.FromRgb(Red.Generate(), Green.Generate(), Blue.Generate());
 		}
 	}
 }
