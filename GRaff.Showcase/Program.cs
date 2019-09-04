48d60bb1dc401a163c2279666a961d9554ae8a7d
marius <Marius_Utheim@hotmail.com>
2019-09-04 15:59:50 +0200

    Add project GRaff.Showcase

---
+++ GRaff.Showcase/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using GRaff;
+
+namespace GRaff.Showcase
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Game.Run();
+        }
+    }
+}
